f3dafa5a5f16ae5ada1bd7156a42d40a477ce203
muhammed ajnas tk <ajnastk432@gmail.com>
2025-05-01 15:32:21 +0530

    join now button dinamic and featured couses component complete

--- components/Faq.js
+++ components/Faq.js
@@ -5,7 +5,7 @@ import { ChevronUp, ChevronDown, X, Plus } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
-
+import Now from "./Now";
 export const FAQHub = () => {
   const [openQuestion, setOpenQuestion] = useState(1);
 
@@ -104,21 +104,9 @@ export const FAQHub = () => {
             {/* <button className="bg-gray-900 text-white px-6 py-2 rounded-full font-medium">
               Start Learning Now
             </button> */}
-            <motion.button
-              className="relative overflow-hidden w-[200px] group py-2 bg-gray-900 text-white rounded-full font-medium hover:bg-gray-800 transition cursor-pointer"
-              initial={{ opacity: 0, scale: 0.9 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{ delay: 1 }} // Optional: adjust delay as needed
-            >
-              <span className="block relative h-10 w-full">
-                <span className="absolute inset-0 flex items-center justify-center transition-all duration-300 transform group-hover:translate-y-full">
-                  Start Learning Now
-                </span>
-                <span className="absolute inset-0 flex items-center justify-center transition-all duration-300 transform -translate-y-full group-hover:translate-y-0">
-                  Start Learning Now
-                </span>
-              </span>
-            </motion.button>
+          <button className="relative overflow-hidden w-52 group py-2 bg-gray-900 text-white rounded-full font-medium hover:bg-gray-800 transition cursor-pointer ">
+                  <Now/>
+                  </button>
           </div>
         </div>
       </div>
